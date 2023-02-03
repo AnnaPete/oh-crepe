0001c5d75be9c27c5c89a0d404f84ae32feaf799
Anna Peterson <reveragb@gmail.com>
2023-02-03 15:48:03 -0700

    Fix error

--- src/MovieTile/MovieTile.jsx
+++ src/MovieTile/MovieTile.jsx
@@ -1,18 +1,18 @@
 import './MovieTile.css'
+// import React, { Component } from 'react'
 
 function MovieTile( movie, key) {
     const currentMovie = movie.movie
     console.log(currentMovie)
     return (
-        <div 
-            className="movie-tile"
-            style={{
-                backgroundImage: `url(${currentMovie["poster_path"]})`
-            }}
-        >
-
-        </div>
+			<div 
+				className="movie-tile"
+				style={{
+					backgroundImage: `url(${currentMovie["poster_path"]})`
+				}}
+			>
+			</div>
     )
-  }
+  }  
   
   export default MovieTile;
