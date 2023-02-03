df6c84a2a27736b338ae193b94b79685d1f56a5c
Anna Peterson <reveragb@gmail.com>
2023-02-03 15:46:55 -0700

    Change to a class instead of a function

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
+import React, { Component } from 'react'
 import Home from '../Home/Home'
 import Header from '../Header/Header'
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  )
+
+class App extends Component {
+  constructor() {
+    super()
+    // this.state = {
+    //   currentMovie: ''
+    // }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
+    )
+  }
+
 }
+
 
 export default App
