3cce9c502865b7bc662c67c691b359d946e64382
Anna Peterson <reveragb@gmail.com>
2023-02-03 17:28:27 -0700

    Create movie page

--- src/MoviePage/MoviePage.jsx
+++ src/MoviePage/MoviePage.jsx
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 import './MoviePage.css'
-import movieData from '../Utilities/movieData'
-class MoviePage extends Component {
-  constructor() {
-    super()
-    this.state = {
-      currentMovie: ''
-    }
-  }
 
-  render() {
-    return (
-      <div className='movie-page'>
-      <h1>hello</h1>
+function MoviePage(props)  {
+  console.log(props)
+  return (
+    <div className='movie-page'>
+      <div className='title'>
+        <h1>{props.currentMovie.title}</h1>
+        <img src={props.currentMovie.poster_path}/>
+      </div>
+      <div className='details'>
+        <p>{props.currentMovie.tagline}</p>
+        <p>Release Date: {props.currentMovie.release_date}</p>
+        <p>Average Rating: {props.currentMovie.average_rating}</p>
+        <p>Runtime: {props.currentMovie.runtime}</p>
+        <p>Overview: {props.currentMovie.overview}</p>
+        <p>Genres: {props.currentMovie.genres}</p>
+        <p>Budget: {props.currentMovie.budget}</p>
+        <p>Revenue: {props.currentMovie.revenue}</p>
+      </div>
     </div>
-    )
-  }
+  )
 }
 
 export default MoviePage;
