1a15ba88de5ffc9bf52d3f207408b33873c7d5aa
Anna Peterson <reveragb@gmail.com>
2023-02-03 17:31:00 -0700

    Add methods to set the state

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,20 +2,31 @@ import './App.css'
 import React, { Component } from 'react'
 import Home from '../Home/Home'
 import Header from '../Header/Header'
+import MoviePage from '../MoviePage/MoviePage'
 
 class App extends Component {
   constructor() {
     super()
-    // this.state = {
-    //   currentMovie: ''
-    // }
+    this.state = {
+      currentMovie: null
+    }
   }
+
+  showMovie = (movie) => {
+    this.setState({currentMovie: movie})
+  }
+
+  returnHome = () => {
+    this.setState({currentMovie: null})
+  } 
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <Home />
+        <Header returnHome={this.returnHome}/>
+        {!this.state.currentMovie 
+        ? <Home showMovie={this.showMovie}/> 
+        : <MoviePage currentMovie={this.state.currentMovie} />} 
       </div>
     )
   }
