514bacf5887d577aee9bae1ae51bb366a34a819f
Anna Peterson <reveragb@gmail.com>
2023-02-03 17:29:54 -0700

    Add props to movie title

--- src/MovieTile/MovieTile.jsx
+++ src/MovieTile/MovieTile.jsx
@@ -1,18 +1,21 @@
 import './MovieTile.css'
-// import React, { Component } from 'react'
 
-function MovieTile( movie, key) {
-    const currentMovie = movie.movie
-    console.log(currentMovie)
-    return (
-			<div 
-				className="movie-tile"
-				style={{
-					backgroundImage: `url(${currentMovie["poster_path"]})`
-				}}
-			>
-			</div>
-    )
-  }  
+function MovieTile( props ) {
+	const currentMovie = props.movie
+	const displayDetails = () => {
+		props.showMovie(currentMovie)
+	}
+
+	return (
+		<div 
+			className="movie-tile"
+			style={{
+				backgroundImage: `url(${currentMovie["poster_path"]})`
+			}}
+			onClick={displayDetails}
+		>
+		</div>
+	)
+}  
   
   export default MovieTile;
