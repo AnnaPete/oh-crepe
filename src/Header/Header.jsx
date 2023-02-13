628a59d0cc5af99ed1c7bb3db457dc9b8f719573
Anna Peterson <reveragb@gmail.com>
2023-02-13 11:54:09 -0700

    Add logo to header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,13 @@
 import './Header.css'
+import image from '../Images/clapper.png'
 import {Link} from 'react-router-dom'
 
 function Header(props) {
     return (
         <div className="header">
-            <Link to={'/'} style={{ textDecoration: 'none' }}>
-                <h1 className='oh-crepe'>Oh Crepe</h1>
+            <Link to={'/'} style={{ textDecoration: 'none' }} className='please'>
+                <img className='logo' src={image} alt='clapper' />
+                <h1 className='oh-crepe'>Oh, Crepe!</h1>
             </Link>
         </div>
     )
