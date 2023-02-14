12d4199915b81561e403d3ae5f30081601dca6af
Anna Peterson <reveragb@gmail.com>
2023-02-13 17:09:54 -0700

    Updates

--- cypress/e2e/404Error-spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('404 Error handling testing', () => {
-
-  it('should display a 404 error when it is encountered', () => {
-    cy.visit('http://localhost:3000/829280')
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { statusCode: 404 })
-    cy.contains('Error 404, movie not found')
-  })
-  
-})
