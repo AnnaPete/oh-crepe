eb5bec5f6dfc760ca5e5d20dfbefc5ba50453078
Zac Walters <zacwalters4@gmail.com>
2023-02-17 13:13:24 -0700

    Add redirect to nonexistent routes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,7 +4,7 @@ import Home from '../Home/Home'
 import Header from '../Header/Header'
 import MoviePage from '../MoviePage/MoviePage'
 import { getMovies } from '../Utilities/APICalls'
-import { Route } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 import { getSingleMovie } from '../Utilities/APICalls'
 
 class App extends Component {
@@ -53,19 +53,22 @@ class App extends Component {
     return (
       <main className="App">
         <Header />
-        <Route exact path="/" render={ () => <Home movieList={this.state.movieList} setError={this.setError} showMovie={this.showMovie}/>}/>
-        <Route exact path="/movie/:id" 
-          render={ ({match}) => {
-              const movieExists = this.state.movieList.find(movie => movie.id === parseInt(match.params.id))
-              if(movieExists) {
-                return <MoviePage currentMovie={match.params.id} setError={this.setError} />  
-              } 
-              else if(!movieExists) {
-                return <h1>This movie does not exist</h1>
+        <Switch>
+          <Route exact path="/" render={ () => <Home movieList={this.state.movieList} setError={this.setError} showMovie={this.showMovie}/>}/>
+          <Route exact path="/movie/:id" 
+            render={ ({match}) => {
+                const movieExists = this.state.movieList.find(movie => movie.id === parseInt(match.params.id))
+                if(movieExists) {
+                  return <MoviePage currentMovie={match.params.id} setError={this.setError} />  
+                } 
+                else if(!movieExists) {
+                  return <h1>This movie does not exist</h1>
+                }
               }
-            }
-          }/>
-        {this.state.error && <p className="error-message">{this.state.error}</p>}
+            }/>
+          {this.state.error && <p className="error-message">{this.state.error}</p>}
+          <Redirect to='/' />
+        </Switch>
       </main>
     )
   }
